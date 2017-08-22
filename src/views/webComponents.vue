9ba3fbc1dc9fcd88b546d8364cf23fc1c8806808
王玮 <316817495@qq.com>
2017-08-22 17:00:49 +0800

    暂存

--- src/views/webComponents.vue
+++ src/views/webComponents.vue
@@ -2,7 +2,7 @@
     <div class="webComponents-view">
         <app-header></app-header>
        
-              <web :src="url" class="webview" :style="{'height':windowHeight}"></web>
+              <web :src="url" class="webview" :style="{'height':windowHeight,'width':windowWidth}"></web>
        
         <div class="loading" v-if="loading">
             <text class="loading-text">{{windowHeight}}</text>
@@ -13,6 +13,8 @@
 <script>
     // const webview = weex.requireModule('webview')
     let windowHeight = (WXEnvironment.deviceHeight -50 )+'px'
+    let windowWidth =WXEnvironment.deviceWidth +'px'
+    
     import AppHeader from '../components/app-header.vue'
     export default {
         components: { AppHeader },
@@ -21,6 +23,7 @@
                 loading: true,
                 url:'https://www.baidu.com/',
                 windowHeight:windowHeight,
+                windowWidth:windowWidth,
             }
         },
         methods:{
@@ -44,11 +47,7 @@
     color: #BBB;
   }
    .webview {
-    /* margin-left: 75px; */
-     width: 100%; 
-    height: 750px;
-    border-width: 2px;
-    /* border-style: solid;
-    border-color: #41B883; */
+   
+  
   }
 </style>
