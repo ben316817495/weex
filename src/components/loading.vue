c3ba48f973cca54bafa00e874439ee69472f49ee
王玮 <316817495@qq.com>
2017-08-24 16:06:23 +0800

    加载动画

---
+++ src/components/loading.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="loading">
+        <!-- {{loadingImg}} -->
+       
+
+        <image class="image" src="http://vue.wangmc.cn/img/loading.gif"></image>   
+       
+         <text class="loading-text">拼命加载中,请稍后...</text> 
+    </div>
+</template>
+
+<script>
+// import loadingImg from '../assets/img/123demo.png'
+// console.log(loadingImg)
+export default {
+    data() {
+        return {
+            //  loadingImg,
+        }
+    }
+}
+</script>
+
+<style scoped>
+.image{
+    height:168px ;
+    width: 160px;
+    position:absolute;
+    top:200px;
+}
+.loading {
+    width: 750px;
+    height: 600px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    /* background-image: url('../assets/img/123demo.png')  */
+}
+
+.loading-text {
+    margin: auto;
+    text-align: center;
+    font-size: 40px;
+    color: #BBB;
+}
+</style>
+  
--- src/views/webComponents.vue
+++ src/views/webComponents.vue
@@ -1,27 +1,32 @@
 <template>
     <div class="webComponents-view">
          <!-- <app-header></app-header>  -->
-       
+        
+        <loading v-if="loading" :style="{'height':windowHeight}"></loading>
+        <networkError v-if="networkError"></networkError>
         <web :src="url" class="webview" :style="{'height':windowHeight,'width':windowWidth}"  @pagestart="start" @pagefinish="finish"></web>
        
-        <!-- <div class="loading" v-if="loading">
-            <text class="loading-text">loading ...</text>
-        </div> -->
+        <!-- -->
     </div>
 </template>
 
 <script>
     // const webview = weex.requireModule('webview')
-    const modal = weex.requireModule('modal')
     let windowHeight = WXEnvironment.deviceHeight+'px'
     let windowWidth =WXEnvironment.deviceWidth +'px'
     
     import AppHeader from '../components/app-header.vue'
+    import loading from '../components/loading.vue'
+    import networkError from '../components/networkError.vue'
+    
+
+
     export default {
-        components: { AppHeader },
+        components: { AppHeader,loading,networkError },
         data () {
             return {
                 loading: true,
+                networkError:false,
                 url:'https://ben316817495.github.io/mixedApplication/showpage/',
                 windowHeight:windowHeight,
                 windowWidth:windowWidth,
@@ -29,29 +34,16 @@
         },
         methods:{
             start(){
-                 modal.toast({ message: 'pagestart' })
             },
             finish (event) {
-                modal.toast({ message: 'pagefinish' })
+                this.loading = false;
             },
+
         },
      }
 </script>
 
 <style scoped>
-    .loading {
-    width: 750px;
-    height: 120px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-  .loading-text {
-    margin: auto;
-    text-align: center;
-    font-size: 40px;
-    color: #BBB;
-  }
    .webview {
    
   
